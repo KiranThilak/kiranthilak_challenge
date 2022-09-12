315c2b22db81024f3c731178e6cb9ec428843462
kiranthilak <kiran.thilak@yahoo.com>
2022-09-12 14:40:55 +0530

    button added

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,40 @@ import {IoRestaurantOutline} from 'react-icons/io5';
 
 const Header = () => {
   return (
+    <>
     <header className='header'>
-      <IoRestaurantOutline />
+      <IoRestaurantOutline style=
+      {{
+        fontSize: '30px',
+        marginRight: "10px",
+      }} />
      
         <h2>Food,s Restaurant</h2>
+
     </header>
+    <main>
+      <h1 style=
+      {{
+        fontSize: '40px',
+        fontFamily: "sans-serif",
+      }}>
+        Welcome to Food's Kitchen
+      </h1>
+
+      <button style=
+      {{
+        background: 'royalblue',
+        color: "white",
+        borderRadius: "3px",
+        fontFamily: "roboto",
+        border:"none",
+        cursor: "pointer",
+        fontSize:"13px",
+        
+      }}>GO TO MENU</button>
+
+    </main>
+    </>
   ) 
 }
 
