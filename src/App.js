986036cd25bbe5bfe3eec33574e626d42ea47b16
kiranthilak <kiran.thilak@yahoo.com>
2022-09-12 17:23:11 +0530

    props drilling completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
+import data from "./data";
+console.log(data)
 
 function App() {
+  const cards = data.map(item => {
+    return (
+        
+         <Main 
+         image={item.image}
+         name = {item.name}
+         price = {item.price}
+   
+         />
+    )
+  })
   return (
     <div className="App">
      {/*  <Header /> */}
-      <Main />
+     {cards}
+
+     
      
     </div>
   );
