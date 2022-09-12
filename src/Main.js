a18a83a689593df57c1e2aa6b50c6ed6b1caf90e
kiranthilak <kiran.thilak@yahoo.com>
2022-09-12 17:25:21 +0530

    props added

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import burger from "./assets/burger.jpeg"
 
-const Main = () => {
+const Main = (props) => {
   return (
     <div className='card'>
-        <img src={burger} className="card-image"/>
+        <img src={props.img} className="card-image"/>
         <div className='card-stats'>
-            <h4>Hamburger</h4>
-            <p>Price:200</p>
+            <h4>{props.name}</h4>
+            <p>Price:{props.price}</p>
+            
             <button>+</button>
             <button>-</button>
-
+            
         </div>
 
     </div>
