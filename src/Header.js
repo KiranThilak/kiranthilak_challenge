55155af3d5f0323be858fce2c7fcca52f185b727
kiranthilak <kiran.thilak@yahoo.com>
2022-09-12 13:52:33 +0530

    Logo added

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {IoRestaurantOutline} from 'react-icons/io5';
+
+
+const Header = () => {
+  return (
+    <header >
+      <IoRestaurantOutline />
+     
+        <h2>Food,s Restaurant</h2>
+    </header>
+  ) 
+}
+
+export default Header
